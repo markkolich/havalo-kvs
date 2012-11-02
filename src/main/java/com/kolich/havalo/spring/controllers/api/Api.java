d5d0c807bd9832e5a63baa6720a3af0c7ad8da7e
Mark Kolich <social@kolich.com>
2012-11-01 23:14:45 -0700

    initial admin user account refactoring

--- src/main/java/com/kolich/havalo/spring/controllers/api/Api.java
+++ src/main/java/com/kolich/havalo/spring/controllers/api/Api.java
@@ -26,32 +26,50 @@
 
 package com.kolich.havalo.spring.controllers.api;
 
+import static com.kolich.havalo.entities.types.HavaloUUID.HAVALO_ADMIN_UUID;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 
-import com.kolich.havalo.spring.controllers.AbstractHavaloController;
+import com.kolich.havalo.entities.types.Repository;
+import com.kolich.havalo.io.managers.RepositoryManager;
 import com.kolich.havalo.spring.controllers.HavaloControllerClosure;
 
 @Controller
-@RequestMapping(value="/")
-public class Index extends AbstractHavaloController {
+@RequestMapping(value="api")
+public class Api extends AbstractHavaloAPIController {
 	
-	private static final Logger logger__ = LoggerFactory.getLogger(Index.class);
+	private static final Logger logger__ = LoggerFactory.getLogger(Api.class);
 	
-	// For WEB-INF/jsp/index-api.jsp
-	private static final String VIEW_NAME = "index-api";
-		
+	@Autowired
+	public Api(RepositoryManager repositoryManager) {
+		super(repositoryManager);
+	}
+	
+	// 1) On startup, "admin repo" is created using empty secret.
+	// 2) HavaloUserDetailsService immeaditely rejects all requests where
+	//    secret in loaded keypair is empty string ..need right error handling
+	//    there too.
+	// 3) Admin consumer calls POST:/api to create and set new secret for admin
+	//    user.  request is rejected if current secret attached to keypair is
+	//    non-empty string.  new keypair is returned to user on call to POST:/api
+	//    user should save this and not lose it.
+	// 4) implement POST:/api to change initial empty string ("") KP secret
+	
 	@RequestMapping(method={RequestMethod.GET, RequestMethod.HEAD})
-	public ModelAndView index() {
-		return new HavaloControllerClosure<ModelAndView>(
+	public ResponseEntity<byte[]> index() {
+		return new HavaloControllerClosure<ResponseEntity<byte[]>>(
 			"[GET,HEAD]:/", logger__) {
 			@Override
-			public ModelAndView doit() throws Exception {
-				return getModelAndView(VIEW_NAME);
+			public ResponseEntity<byte[]> doit() throws Exception {
+				final Repository repo = getRepository(HAVALO_ADMIN_UUID);
+				return getJsonResponseEntity(repo.getKeyPair(), HttpStatus.OK);
 			}
 		}.execute();
 	}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 
-<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
-
-<% response.sendRedirect("api/"); %>
+<html><body>
+<h2>Havalo</h2>
+</body></html>
