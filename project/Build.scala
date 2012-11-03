0fe84e4e584f2e6266c88a37f2152b39322fa51c
Mark Kolich <social@kolich.com>
2012-11-03 16:45:44 -0700

    repository deletion support

--- project/Build.scala
+++ project/Build.scala
@@ -98,7 +98,7 @@ object Common extends Build {
   import Resolvers._
 
   private val aName = "havalo"
-  private val aVer = "0.0.1"
+  private val aVer = "0.0.2"
   private val aOrg = "com.kolich"
 
   lazy val havalo: Project = Project(
