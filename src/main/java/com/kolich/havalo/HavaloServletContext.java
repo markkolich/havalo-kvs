4ef40d304d87be6f054d52a80bc1a770aa4bc1f6
Mark Kolich <social@kolich.com>
2013-03-26 13:18:30 -0700

    got typesafe configuration fallback to work correctly

--- src/main/java/com/kolich/havalo/HavaloServletContext.java
+++ src/main/java/com/kolich/havalo/HavaloServletContext.java
@@ -27,6 +27,7 @@
 package com.kolich.havalo;
 
 import static com.kolich.havalo.entities.types.UserRole.ADMIN;
+import static java.lang.System.getProperty;
 
 import java.io.File;
 import java.util.Arrays;
@@ -73,6 +74,11 @@ public final class HavaloServletContext implements ServletContextListener {
 	
 	private static final String REPO_BASE_DEFAULT = "WEB-INF/work";
 	
+	private static final String HAVALO_EXTERNAL_CONFIG_FILENAME = "havalo.conf";
+	private static final String CONTAINER_CONF_DIRNAME = "conf";
+	private static final String JETTY_HOME_SYS_PROPERTY = getProperty("jetty.home");
+	private static final String CATALINA_HOME_SYS_PROPERTY = getProperty("catalina.home");
+	
 	private ServletContext context_;
 			
 	@Override
@@ -80,20 +86,33 @@ public final class HavaloServletContext implements ServletContextListener {
 		logger__.info("Servlet context initialized.");
 		// Get the default servlet context.
 		context_ = event.getServletContext();
+		final Config refConfConfig = ConfigFactory.load();		
+		// Load the external 'havalo.conf' application configuration file
+		// specific to the internal Servlet container.
+		final Config overrideConfig = getOverrideHavaloConfig();
 		// Load and build the application configuration, then attach the
 		// loaded immutable config to the servlet context. 
-		final Config config = ConfigFactory.load();
-		context_.setAttribute(HAVALO_CONTEXT_REPO_MANAGER_ATTRIBUTE, config);
-		for(final Map.Entry<String,ConfigValue> entry : config.entrySet()) {
+		final Config havaloConfig;
+		if(overrideConfig != null) {
+			logger__.debug("Found valid override configuration; " +
+				"using override.");
+			havaloConfig = overrideConfig.withFallback(refConfConfig);
+		} else {
+			logger__.debug("Found no valid override configuration; " +
+				"using default configuration provided by bundled " +
+				"reference.conf");
+			havaloConfig = refConfConfig;
+		}
+		for(final Map.Entry<String,ConfigValue> entry : havaloConfig.entrySet()) {
 		    logger__.trace("Loaded config (key=" + entry.getKey() +
 		    	", value=" + entry.getValue() + ")");
 		}
 		// Attach the underlying configuration to the servlet context too.
-		context_.setAttribute(HAVALO_CONTEXT_CONFIG_ATTRIBUTE, config);
+		context_.setAttribute(HAVALO_CONTEXT_CONFIG_ATTRIBUTE, havaloConfig);
 		// Create a new repository manager based on the desired
 		// underlying repository root directory on disk.
 		final RepositoryManager repoManager =
-			createInitialAdminRepository(context_, config);
+			createInitialAdminRepository(context_, havaloConfig);
 		context_.setAttribute(HAVALO_CONTEXT_REPO_MANAGER_ATTRIBUTE,
 			repoManager);
 		// Create a new user lookup (auth) service.
@@ -190,6 +209,50 @@ public final class HavaloServletContext implements ServletContextListener {
 	private static final HavaloUserService createUserService(
 		final RepositoryManager repoManager) {
 		return new HavaloUserService(repoManager);
+	}
+	
+	private static final Config getOverrideHavaloConfig() {
+		File configFile = null;
+		if(JETTY_HOME_SYS_PROPERTY != null) {
+			final File jettyConfigFile = new File(
+				new File(JETTY_HOME_SYS_PROPERTY, CONTAINER_CONF_DIRNAME),
+				HAVALO_EXTERNAL_CONFIG_FILENAME);
+			if(jettyConfigFile.exists()) {
+				logger__.info("Found Jetty specific " +
+					HAVALO_EXTERNAL_CONFIG_FILENAME + " configuration file " +
+					"at: " + jettyConfigFile.getAbsolutePath());
+				configFile = jettyConfigFile;
+			}
+		} else if(CATALINA_HOME_SYS_PROPERTY != null) {
+			final File catalinaConfigFile = new File(
+				new File(CATALINA_HOME_SYS_PROPERTY, CONTAINER_CONF_DIRNAME),
+				HAVALO_EXTERNAL_CONFIG_FILENAME);
+			if(catalinaConfigFile.exists()) {
+				logger__.info("Found Catalina (Tomcat) specific " +
+					HAVALO_EXTERNAL_CONFIG_FILENAME + " configuration file " +
+					"at: " + catalinaConfigFile.getAbsolutePath());
+				configFile = catalinaConfigFile;
+			}
+		}
+		Config config = null;
+		try {
+			// If we found a valid config file specific to the supported
+			// Servlet container, then load it.  Otherwise, return null meaning
+			// no override file was found.
+			config = (configFile != null) ?
+				// Load the external 'havalo.conf' Typesafe configuration
+				// from an external file.
+				ConfigFactory.parseFile(configFile) :
+				// No valid external config file found, return null.
+				null;
+		} catch (Exception e) {
+			// Usually get here when the provided havalo.conf override file
+			// is malformed or something went wrong while loading it.
+			logger__.warn("Failed to parse override " +
+				HAVALO_EXTERNAL_CONFIG_FILENAME + " configuration file.", e);
+			config = null;
+		}
+		return config;
 	}
 		
 }
